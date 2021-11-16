dd926431e0d07ac1fb56e56f3aedd53190c68359
vegxer <vegxer@mail.ru>
2021-11-16 22:51:39 +0300

    Добавьте файлы проекта.

---
+++ SnakeConsole/SnakeConsole.cpp
@@ -0,0 +1,193 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
+
+const int width = 20, height = 20;
+bool game = true, yes = false;
+string mover;
+int i = 5, j = 10, fruitI = rand() % (height - 2) + 1, fruitJ = rand() % (width - 2) + 1, score = 0, snake[102], n = 4, c = -2;
+char root[height][width] =
+{
+    {'_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z'}
+};
+
+
+void write()
+{
+    for (int p = 0; p < n; p += 2)
+    {
+        if (p == 0)
+            root[snake[p]][snake[p + 1]] = 'o';
+        else
+            root[snake[p]][snake[p + 1]] = '#';
+    }
+    Sleep(90);
+    HANDLE hd = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD cd;
+    cd.X = 0;
+    cd.Y = 0;
+    SetConsoleCursorPosition(hd, cd);
+    for (int k = 0; k < height; k++)
+    {
+        for (int l = 0; l < width; l++)
+            if (k == fruitI && l == fruitJ)
+                cout << '*';
+            else
+                cout << root[k][l];
+        cout << "\n";
+    }
+    cout << "score: " << score << "\n";
+}
+
+void input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            mover = "LEFT";
+            break;
+        case 'd':
+            mover = "RIGHT";
+            break;
+        case 'w':
+            mover = "UP";
+            break;
+        case 's':
+            mover = "DOWN";
+            break;
+        case 'e':
+            game = false;
+            break;
+        }
+    }
+}
+
+void fruit()
+{
+    if (snake[0] == fruitI && snake[1] == fruitJ)
+    {
+        do
+        {
+            fruitI = rand() % (height - 2) + 1;
+            fruitJ = rand() % (width - 2) + 1;
+        } while (root[fruitI][fruitJ] == '#');
+        score += 1;
+        n += 2;
+        if (snake[n - 4] > snake[n - 6])
+            snake[n - 2] = snake[n - 4] + 1;
+        else
+            if (snake[n - 4] < snake[n - 6])
+                snake[n - 2] = snake[n - 4] - 1;
+            else
+                if (snake[n - 3] > snake[n - 5])
+                    snake[n - 1] = snake[n - 3] + 1;
+                else
+                    if (snake[n - 3] < snake[n - 5])
+                        snake[n - 1] = snake[n - 3] - 1;
+    }
+}
+
+void move()
+{
+    if (mover == "UP")
+    {
+        for (int i = n - 1; i >= 2; i--)
+            snake[i] = snake[i - 2];
+        snake[0] -= 1;
+    }
+    else
+        if (mover == "DOWN")
+        {
+            for (int i = n - 1; i >= 2; i--)
+                snake[i] = snake[i - 2];
+            snake[0] += 1;
+        }
+        else
+            if (mover == "LEFT")
+            {
+                for (int i = n - 1; i >= 2; i--)
+                    snake[i] = snake[i - 2];
+                snake[1] -= 1;
+            }
+            else
+                if (mover == "RIGHT")
+                {
+                    for (int i = n - 1; i >= 2; i--)
+                        snake[i] = snake[i - 2];
+                    snake[1] += 1;
+                }
+    if (root[snake[0]][snake[1]] == '#')
+    {
+        system("cls");
+        cout << " ####   ####  #   # #####\n";
+        cout << "##     ##  ## ## ## #\n";
+        cout << "## ### ###### # # # ####\n";
+        cout << "##  ## ##  ## #   # ##\n";
+        cout << " ####  ##  ## #   # #####\n\n";
+        cout << " ####  ##  ## #####  #####\n";
+        cout << "##  ## ##  ## ##     ##  ##\n";
+        cout << "##  ## ##  ## ####   #####\n";
+        cout << "##  ##  ####  ##     ##  ##\n";
+        cout << " ####    ##   #####  ##  ##\n\n";
+        cout << "your score: " << score << endl;
+        game = false;
+    }
+    for (int c = 0; c < n; c++)
+        if (root[snake[c]][snake[c + 1]] == '_' || root[snake[c]][snake[c + 1]] == '|' || root[snake[c]][snake[c + 1]] == 'Z')
+        {
+            if (snake[c] < 1)
+                snake[c] = height - 2;
+            if (snake[c] > height - 2)
+                snake[c] = 1;
+            if (snake[c + 1] < 1)
+                snake[c + 1] = width - 2;
+            if (snake[c + 1] > width - 2)
+                snake[c + 1] = 1;
+        }
+    for (int k = 0; k < height; k++)
+        for (int l = 0; l < width; l++)
+            if (root[k][l] == '#')
+                root[k][l] = ' ';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    setlocale(LC_CTYPE, "rus");
+    srand(time(NULL));
+    snake[0] = 5, snake[1] = 10, snake[2] = 6, snake[3] = 10;
+    while (game == true)
+    {
+        write();
+        input();
+        fruit();
+        move();
+    }
+    system("pause");
+    return 0;
+}
---
+++ TanksConsole/TanksConsole.cpp
@@ -0,0 +1,258 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <random>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
+
+int shirina = 20, dlina = 40, ship_health = 10, shot[20][2], bots[20][8], bot_shot[30][2], score = -20, bot_shot_max = 30, bot_shot_count = -1, count_shot = -1, max_shot = 10, placeI = 2, count_bot = -1, max_bot = 20, bot_xod = 1, base_health = 20;
+
+int tank[4][2];
+
+char a[20][40] =
+{
+    {'_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+    {'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z'},
+};
+
+void draw()
+{
+    for (int i = 0; i < max_shot; ++i)
+        for (int j = 0; j < max_bot; ++j)
+            if ((shot[i][0] == bots[j][0] || shot[i][0] == bots[j][6]) && (shot[i][1] == bots[j][1] || shot[i][1] == bots[j][3] || shot[i][1] == bots[j][5]))
+            {
+                ++score;
+                shot[i][0] = 0, shot[i][1] = 0;
+                for (int k = 0; k <= 6; k += 2)
+                {
+                    bots[j][k] = 0;
+                    bots[j][k + 1] = dlina - 1;
+                }
+            }
+    for (int i = 0; i < max_bot; ++i)
+    {
+        a[bots[i][0]][bots[i][1]] = '@';
+        a[bots[i][2]][bots[i][3]] = '@';
+        a[bots[i][4]][bots[i][5]] = '@';
+        a[bots[i][6]][bots[i][7]] = '@';
+    }
+    for (int i = 0; i < bot_shot_max; ++i)
+        a[bot_shot[i][0]][bot_shot[i][1]] = '+';
+    for (int i = 0; i < 4; ++i)
+        a[tank[i][0]][tank[i][1]] = '$';
+    for (int i = 0; i < max_shot; ++i)
+        a[shot[i][0]][shot[i][1]] = '*';
+    Sleep(70);
+    HANDLE hd = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD cd;
+    cd.X = 0;
+    cd.Y = 0;
+    SetConsoleCursorPosition(hd, cd);
+    for (int i = 0; i < shirina; ++i)
+    {
+        for (int j = 0; j < dlina; ++j)
+        {
+            cout << a[i][j];
+            if (i != 0 && i != shirina - 1 && j != 0 && j != dlina - 1)
+                a[i][j] = ' ';
+        }
+        cout << '\n';
+    }
+    cout << "\nHealth of base: " << base_health;
+    cout << "\nHealth of tank: " << ship_health;
+    cout << "\nScore: " << score;
+}
+
+void set_tank()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            if (tank[1][1] - 1 > 0)
+            {
+                for (int i = 0; i < 4; ++i)
+                    --tank[i][1];
+            }
+            break;
+        case 's':
+            if (tank[1][0] + 1 < shirina - 1)
+            {
+                for (int i = 0; i < 4; ++i)
+                    ++tank[i][0];
+            }
+            break;
+        case 'd':
+            if (tank[3][1] + 1 < dlina - 1)
+            {
+                for (int i = 0; i < 4; ++i)
+                    ++tank[i][1];
+            }
+            break;
+        case 'w':
+            if (tank[0][0] - 1 > 0)
+            {
+                for (int i = 0; i < 4; ++i)
+                    --tank[i][0];
+            }
+            break;
+        case 32:
+            ++count_shot;
+            if (count_shot > max_shot - 1)
+                count_shot = 0;
+            shot[count_shot][0] = tank[0][0];
+            shot[count_shot][1] = tank[0][1];
+            break;
+        }
+    }
+}
+
+void shot_move()
+{
+    for (int i = 0; i < max_shot; ++i)
+        if (shot[i][0] != 0 && shot[i][1] != 0)
+        {
+            if (shot[i][0] > 2)
+                --shot[i][0];
+            else
+                shot[i][0] = 0, shot[i][1] = 0;
+        }
+}
+
+void bot_spawn()
+{
+    int t = rand() % 28;
+    if (t == 8)
+    {
+        int placeJ = (rand() % 38) + 2;
+        if (placeJ > 37)
+            --placeJ;
+        if (placeJ > 37)
+            --placeJ;
+        ++count_bot;
+        if (count_bot > 19)
+            count_bot = 0;
+        bots[count_bot][0] = placeI, bots[count_bot][1] = placeJ - 1, bots[count_bot][2] = placeI, bots[count_bot][3] = placeJ,
+            bots[count_bot][4] = placeI, bots[count_bot][5] = placeJ + 1, bots[count_bot][6] = placeI + 1, bots[count_bot][7] = placeJ;
+    }
+}
+
+void bot_move()
+{
+    ++bot_xod;
+    if (bot_xod > 1000000)
+        bot_xod = 1;
+    if (count_bot > -1 && bot_xod % 7 == 0)
+    {
+        for (int i = 0; i < max_bot; ++i)
+            if (bots[i][6] < shirina - 2 && bots[i][0] != 0 && bots[i][1] != shirina - 1)
+            {
+                for (int j = 0; j <= 6; j += 2)
+                    ++bots[i][j];
+            }
+            else
+            {
+                if (bots[i][6] >= shirina - 2)
+                    --base_health;
+                for (int j = 0; j <= 6; j += 2)
+                {
+                    bots[i][j] = 0;
+                    bots[i][j + 1] = dlina - 1;
+                }
+            }
+    }
+}
+
+void bot_fire()
+{
+    for (int i = 0; i < max_bot; ++i)
+    {
+        int p = rand() % 35;
+        if (bots[i][0] > 0 && bots[i][0] < shirina - 4 && p == 4)
+        {
+            ++bot_shot_count;
+            if (bot_shot_count > 29)
+                bot_shot_count = 0;
+            bot_shot[bot_shot_count][0] = bots[i][6] + 1;
+            bot_shot[bot_shot_count][1] = bots[i][7];
+        }
+    }
+}
+
+void bot_fire_move()
+{
+    for (int i = 0; i < bot_shot_max; ++i)
+    {
+        if (bot_shot[i][0] < shirina - 1 && bot_shot[i][1] > 0)
+        {
+            if ((bot_shot[i][0] == tank[0][0] || bot_shot[i][0] == tank[1][0]) && (bot_shot[i][1] == tank[0][1] || bot_shot[i][1] == tank[1][1] || bot_shot[i][1] == tank[2][1] || bot_shot[i][1] == tank[3][1]))
+            {
+                --ship_health;
+                bot_shot[i][0] = shirina - 1;
+                bot_shot[i][1] = 0;
+            }
+            if (bot_shot[i][0] < shirina - 2)
+            {
+                if (bot_xod % 2 == 0)
+                    ++bot_shot[i][0];
+            }
+            else
+            {
+                bot_shot[i][0] = shirina - 1;
+                bot_shot[i][1] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    tank[0][0] = 17, tank[0][1] = 19, tank[1][0] = 18, tank[1][1] = 18, tank[2][0] = 18, tank[2][1] = 19, tank[3][0] = 18, tank[3][1] = 20;
+    shot[0][0] = 0, shot[0][1] = 0;
+    //system("color F0"); белый фон, чёрный текст
+    while (base_health > 0 && ship_health > 0)
+    {
+        draw();
+        set_tank();
+        shot_move();
+        bot_spawn();
+        bot_move();
+        bot_fire();
+        bot_fire_move();
+    }
+    system("cls");
+    cout << " ####   ####  #   # #####\n";
+    cout << "##     ##  ## ## ## #\n";
+    cout << "## ### ###### # # # ####\n";
+    cout << "##  ## ##  ## #   # ##\n";
+    cout << " ####  ##  ## #   # #####\n\n";
+    cout << " ####  ##  ## #####  #####\n";
+    cout << "##  ## ##  ## ##     ##  ##\n";
+    cout << "##  ## ##  ## ####   #####\n";
+    cout << "##  ##  ####  ##     ##  ##\n";
+    cout << " ####    ##   #####  ##  ##\n\n";
+    cout << "Your score: " << score << '\n';
+    system("pause");
+    return 0;
+}
